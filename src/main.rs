86ee048a39eee49c22e4921db9c000ff44352e14
David Venhoek <david@tweedegolf.com>
2021-01-15 08:25:15 +0100

    Implemented jwt signing.

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use clap::Clap;
 mod yubikey;
 use yubikey::YubiKey;
 use signature::Signer;
+use serde::Serialize;
 
 #[derive(Clap, Debug)]
 #[clap(version = "0.1")]
@@ -19,6 +20,19 @@ enum SubCommand {
     Sign,
 }
 
+#[derive(Serialize)]
+struct JWTHeader {
+    alg: String,
+    typ: String,
+    kid: String,
+}
+
+#[derive(Serialize)]
+struct Claims {
+    iat: i64,
+    exp: i64,
+}
+
 fn main() {
     let opts: Opts = Opts::parse();
 
@@ -28,12 +42,27 @@ fn main() {
                 println!("Unable to reach yubikey ({:?})", error);
                 process::exit(1);
             });
-        
             yubikey.verify_pin_from_tty().expect("Failed pin check");
+
+            let header = JWTHeader{
+                alg: String::from("RS256"),
+                typ: String::from("JWT"),
+                kid: String::from(""),
+            };
+
+            let encoded_header = base64::encode_config(serde_json::to_string(&header).unwrap(), base64::URL_SAFE_NO_PAD);
+
+            let claim = Claims {
+                iat: 5,
+                exp: 3605,
+            };
+
+            let encoded_claim = base64::encode_config(serde_json::to_string(&claim).unwrap(), base64::URL_SAFE_NO_PAD);
+
+            let jwt_signature_input = format!("{}.{}", &encoded_header, &encoded_claim);
         
-            let data: Vec<u8> = vec![3,1,2,5,7,9,1];
-            let res = yubikey.sign(&data);
-            println!("{:?}", res);
+            let res = yubikey.sign(jwt_signature_input.as_bytes());
+            println!("{}.{}.{}", &encoded_header, &encoded_claim, base64::encode_config(res, base64::URL_SAFE_NO_PAD));
         },
     }
 }
